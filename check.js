3a2adc9d7c3a4ba5d62cbccd2cf07aff4a12240a
ceepvoice <53182786+ceepvoice@users.noreply.github.com>
2019-08-24 06:17:15 -0700

    Update check.js

--- check.js
+++ check.js
@@ -1,9 +1,21 @@
 function themeInfo(a){
  var info = { 
- "name": "NullShell",
- "coder": "i.aSalt",
- "version":"1.1.0",
- "newVersion":"false"
+ "name": "NullShell", // Template Name
+ "coder": "i.aSalt", // Who wrote it ?
+ "version":"1.1.0", // Present Version 
+ "cost":"300.000", // Self Support Cost
+ "newVersion": false , // New Version
+ "update": false , // Free update
+ "uVersion" : false , // New Free Update Version 
+ "uNew" : false , // What is new free update version have ?
+ "pUpdate": false , // Paid update
+ "pVersion": false ,  // New Paid Update Version 
+ "pCost":false , // Paid Update Version Cost
+ "pNew":false, // What is new paid version have ?
  };
+if ( info[a] == false ) {
+ return false } 
+ else {
  return info[a]
+ }
 }
